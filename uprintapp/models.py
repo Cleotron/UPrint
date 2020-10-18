5356c1b62468f09c7c33a2de049d6f24ea1bdeea
monamutees <monamutees@gmail.com>
2020-10-18 17:04:02 +0200

    Limpieza de merges

--- uprintapp/models.py
+++ uprintapp/models.py
@@ -8,7 +8,6 @@ class Pyme:
 
     def calcularHuella(self,args):
         # calculamos la huella de la pyme con el consumo que nos proporciona el usuario multiplicado por el factor
-<<<<<<< Updated upstream
         h_luz = args[0]*0.001*0.505*12
         h_agua = args[1]*1*6
         h_gas=args[2]*11.7*12
@@ -38,39 +37,3 @@ class Pyme_DB(models.Model):
     coste_luz = models.FloatField()
     l_gas = models.IntegerField()
     coste_gas = models.FloatField()
-=======
-        huella = args[0]*0.001*0.505 + args[1]*1 + args[2]*1
-
-        return huella
-"""
-    def compararHuella(self, sector):
-        self.huella
-        #conectamos con la bd para comparar la huella de nuestra pyme con la media del sector
-        connection = """
->>>>>>> Stashed changes
-
-
-import cx_Oracle
-
-
-class Consumo:
-    def __init__(self):
-        self.connection = cx_Oracle.connect("system", "password", "localhost/XE")
-
-    def altaConsumo(self, args):
-        cursor = self.connection.cursor()
-
-        # name, empleados, sector, consumo_luz, consumo_agua, consumo_gas
-        try:
-            alta = ("INSERT INTO tabla VALUES(:p1, :p2, :p3, :p4, :p5, :p6)")
-            cursor.execute(alta, args)
-            reg = cursor.rowcount
-            if reg>0:
-                print("Hemos guardado sus datos correctamente")
-                self.connection.commit()
-
-        except self.connection.Error as error:
-            print("Error: ", error)
-
-        return reg
-    
